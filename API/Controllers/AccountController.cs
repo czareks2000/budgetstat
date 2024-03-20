9815907ba6ddb04b249b1c1dbf799d9372bb2e4d
Cezary Stachurski <czareks20006@gmail.com>
2024-03-20 12:22:23 +0100

    Clean up

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/account/")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        [HttpGet("all")]
+        public IActionResult Accounts()
+        {
+            return Ok();
+        }
+    }
+}
