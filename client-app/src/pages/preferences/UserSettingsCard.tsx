cb0d3e98e5c0df710d68af11720bd183df69849d
Cezary Stachurski <czareks20006@gmail.com>
2024-07-05 17:46:32 +0200

    placeholder

--- client-app/src/pages/preferences/UserSettingsCard.tsx
+++ client-app/src/pages/preferences/UserSettingsCard.tsx
@@ -1,11 +1,13 @@
-import { Box, Card, CardContent } from "@mui/material"
+import { Box, Card, CardContent, Typography } from "@mui/material"
 import { observer } from "mobx-react-lite"
 
 export default observer(function UserSettingsCard() {
   return (
     <Card>
         <CardContent>
-            <Box p={2}></Box>
+            <Box>
+                <Typography variant="body1">Tu będzie formularz zmiany hasła</Typography>
+            </Box>
         </CardContent>
     </Card>
 )
