90e4576b74b3ec2d49abb779934e3826f2ce1abd
Cezary Stachurski <czareks20006@gmail.com>
2024-08-16 09:30:51 +0200

    add active class when location is /

--- client-app/src/app/layout/menu/NavLinks.tsx
+++ client-app/src/app/layout/menu/NavLinks.tsx
@@ -1,6 +1,7 @@
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { theme } from "../Theme";
 import NoDecorationNavLink from "../../../components/common/NoDecorationNavLink";
+import { useLocation } from "react-router-dom";
 
 interface NavLinkItem{
     text: string;
@@ -13,35 +14,42 @@ interface Props {
   }
   
   const NavLinks = ({ items }: Props) => {
+    const location = useLocation();
+
     return (
         <List>
-            {items.map((item, index) => (
-            <NoDecorationNavLink key={index} to={item.link} content={
-                <ListItem disablePadding>  
-                    <ListItemButton disableRipple
-                        sx={{
-                            '&:hover': {
-                                '& .MuiListItemText-primary': {
-                                    color: theme.palette.primary.main,
+            {items.map((item, index) => {
+
+            const isActive = location.pathname === '/' && item.text === 'Home';
+                
+            return(
+                <NoDecorationNavLink key={index} to={item.link} 
+                    className={isActive ? 'active' : ''} content={
+                    <ListItem disablePadding>  
+                        <ListItemButton disableRipple
+                            sx={{
+                                '&:hover': {
+                                    '& .MuiListItemText-primary': {
+                                        color: theme.palette.primary.main,
+                                    },
+                                    '& .MuiListItemIcon-root': {
+                                        color: theme.palette.primary.main,
+                                    },
                                 },
-                                '& .MuiListItemIcon-root': {
-                                    color: theme.palette.primary.main,
-                                },
-                            },
-                        }}>
-                        <ListItemIcon 
-                            className="MuiListItemIcon-root"
-                            sx={{ color: 'white'}}>
-                            {item.icon}
-                        </ListItemIcon>
-                        <ListItemText primary={item.text} 
-                            primaryTypographyProps={{
-                                className: 'MuiListItemText-primary',
-                            }}/>
-                    </ListItemButton>
-                </ListItem>
-            }/>
-        ))}
+                            }}>
+                            <ListItemIcon 
+                                className="MuiListItemIcon-root"
+                                sx={{ color: 'white'}}>
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.text} 
+                                primaryTypographyProps={{
+                                    className: 'MuiListItemText-primary',
+                                }}/>
+                        </ListItemButton>
+                    </ListItem>
+                }/>)
+        })}
         </List>
     )
   }
--- client-app/src/components/common/NoDecorationNavLink.tsx
+++ client-app/src/components/common/NoDecorationNavLink.tsx
@@ -4,14 +4,16 @@ import { Link as MuiLink } from "@mui/material"
 interface Props {
     to: string;
     content: React.ReactNode;
+    className?: string;
 }
 
-const NoDecorationNavLink = ({to, content}: Props) => {
+const NoDecorationNavLink = ({to, content, className}: Props) => {
   return (
     <MuiLink 
         component={NavLink} 
         to={to}
         underline='none' color='inherit'
+        className={className}
         >
             {content}
     </MuiLink>
