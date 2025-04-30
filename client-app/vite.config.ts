639758c19065afcd81d63345cd8af36a650f917a
Cezary Stachurski <cstachurski@lbpro.pl>
2025-04-30 15:51:04 +0200

    Add PWA

--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,24 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'BudgetStat',
+          short_name: 'BudgetStat',
+          start_url: '/',
+          display: 'standalone',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          icons: [
+            {
+              src: 'icons/icon-600x600.png',
+              sizes: '600x600',
+              type: 'image/png',
+            }
+          ]
+        }
+      })
+]
 })
